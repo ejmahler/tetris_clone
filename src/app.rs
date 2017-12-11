c2e3c5e74710ff3f022fc42bb3721f328858329a
emahler <join.together@gmail.com>
2017-12-10 22:29:40 -0800

    Split the app into its own module

---
+++ src/app.rs
@@ -0,0 +1,83 @@
+
+use piston::event_loop::EventSettings;
+use piston_window::{PistonWindow, RenderArgs, AfterRenderArgs, UpdateArgs, IdleArgs, Input, Event, Loop, rectangle, clear};
+use num_traits;
+
+use game_input::TetrisInput;
+
+pub struct TetrisApp {
+    window: PistonWindow,
+    input: TetrisInput,
+    red: f32,
+    green: f32,
+}
+
+impl TetrisApp {
+    pub fn new(window: PistonWindow) -> Self {
+        Self {
+            window,
+            input: TetrisInput::new(),
+            red: 0.0,
+            green: 0.0,
+        }
+    }
+
+    pub fn create_event_settings(&self) -> EventSettings {
+        let mut settings = EventSettings::new();
+        settings.max_fps = 30;
+        settings.ups = 30;
+
+        settings
+    }
+
+    pub fn get_window<'a>(&'a mut self) -> &'a mut PistonWindow {
+        &mut self.window
+    }
+
+
+
+
+    pub fn render(&mut self, event: RenderArgs) {
+        let placation_event = Event::from(Loop::from(event));
+        let red = self.red;
+        let green = self.green;
+
+        self.window.draw_2d(&placation_event, |context, graphics| {
+            clear([1.0; 4], graphics);
+            rectangle([red, green, 0.0, 1.0], // red
+                        [0.0, 0.0, 100.0, 100.0],
+                        context.transform,
+                        graphics);
+        });
+    }
+    pub fn after_render(&mut self, _: AfterRenderArgs) {
+
+    }
+
+    pub fn update(&mut self, args: UpdateArgs) {
+        let dt = args.dt as f32;
+
+        // update our input axes
+        self.input.update(dt);
+        
+        // if the player pressed the left arrow this frame only, set red to 1
+        if self.input.arrow_left.pressed_this_frame() {
+            self.red = 1.0;
+        } else {
+            self.red = num_traits::clamp(self.red - dt, 0.0, 1.0);
+        }
+
+        // if the player is holding the right arrow, fade to green
+        if self.input.arrow_right.pressed() {
+            self.green = num_traits::clamp(self.green + dt, 0.0, 1.0);
+        } else {
+            self.green = num_traits::clamp(self.green - dt, 0.0, 1.0);
+        }
+    }
+    pub fn idle(&mut self, _: IdleArgs) {
+
+    }
+    pub fn handle_input(&mut self, args: Input) {
+        self.input.handle_input(&args);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,29 @@
 extern crate piston_window;
-use piston_window::{WindowSettings, RenderArgs, Loop, UpdateArgs, Event, IdleArgs, AfterRenderArgs, PistonWindow, Input, rectangle, clear};
+extern crate piston;
+extern crate num_traits;
 
+use piston::event_loop::Events;
+use piston_window::{WindowSettings, Loop, Event};
+
+mod app;
 mod game_input;
 
-struct TetrisApp {
-    window: PistonWindow,
-    input: game_input::TetrisInput,
-    red: f32,
-    green: f32,
-}
+use app::TetrisApp;
 
 fn main() {
-    let mut app = TetrisApp {
-        window: WindowSettings::new("Hello Piston!", [640, 480]).exit_on_esc(true).build().unwrap(),
-        input: game_input::TetrisInput::new(),
-        red: 1.0,
-        green: 0.0,
-    };
+    let window = WindowSettings::new("Hello Piston!", [640, 480]).exit_on_esc(true).build().unwrap();
+    let mut app = TetrisApp::new(window);
 
+    let mut events = Events::new(app.create_event_settings());
 
-    while let Some(event) = app.window.next() {
+    while let Some(event) = events.next(app.get_window()) {
         match event {
-            Event::Loop(Loop::Render(args)) => event_render(args, &mut app),
-            Event::Loop(Loop::AfterRender(args)) => event_after_render(args, &mut app),
-            Event::Loop(Loop::Update(args)) => event_update(args, &mut app),
-            Event::Loop(Loop::Idle(args)) => event_idle(args, &mut app),
-            Event::Input(args) => event_input(args, &mut app),
+            Event::Loop(Loop::Render(args)) => app.render(args),
+            Event::Loop(Loop::AfterRender(args)) => app.after_render(args),
+            Event::Loop(Loop::Update(args)) => app.update(args),
+            Event::Loop(Loop::Idle(args)) => app.idle(args),
+            Event::Input(args) => app.handle_input(args),
             other => println!("got unknown event: {:?}", other),
         }
     }
-}
-
-fn event_render(event: RenderArgs, app: &mut TetrisApp) {
-    let placation_event = Event::from(Loop::from(event));
-    let red = app.red;
-    let green = app.green;
-
-    app.window.draw_2d(&placation_event, |context, graphics| {
-        clear([1.0; 4], graphics);
-        rectangle([red, green, 0.0, 1.0], // red
-                    [0.0, 0.0, 100.0, 100.0],
-                    context.transform,
-                    graphics);
-    });
-}
-fn event_after_render(_: AfterRenderArgs, _: &mut TetrisApp) {
-
-}
-
-fn clamp<T: PartialOrd + Copy>(value: T, min: T, max: T) -> T {
-    if value < min {
-        min
-    } else if value > max {
-        max
-    } else {
-        value
-    }
-}
-
-fn event_update(args: UpdateArgs, app: &mut TetrisApp) {
-    let dt = args.dt as f32;
-
-    // update our input axes
-    app.input.update(dt);
-    
-    // if the player pressed the left arrow this frame only, set red to 1
-    if app.input.arrow_left.pressed_this_frame() {
-        app.red = 1.0;
-    } else {
-        app.red = clamp(app.red - dt, 0.0, 1.0);
-    }
-
-    // if the player is holding the right arrow, fade to green
-    if app.input.arrow_right.pressed() {
-        app.green = clamp(app.green + dt, 0.0, 1.0);
-    } else {
-        app.green = clamp(app.green - dt, 0.0, 1.0);
-    }
-}
-fn event_idle(_: IdleArgs, _: &mut TetrisApp) {
-
-}
-fn event_input(args: Input, app: &mut TetrisApp) {
-    app.input.handle_input(&args);
 }
